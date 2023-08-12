7aa1b86157e3f1f4fed659b2e59debd4db62cf94
David Blot <d.nguyenblot@gmail.com>
2023-08-11 20:32:52 -0500

    Ajout footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 // appel de la fonction pour lancer le jeu :
-lancerJeu();
+// lancerJeu();
+
+// let inputEcriture = document.getElementById('inputEcriture');
+// let btnValiderMot = document.getElementById('btnValiderMot');
+
+// let zoneProposition = document.querySelector('.zoneProposition');
+// let spanScore = document.querySelector('.zoneScore span');
+
+// let listeBtnRadio = document.querySelectorAll('.optionSource input');
