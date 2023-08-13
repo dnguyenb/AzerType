13e0cc2335b03aff961b1fcc5a28d686c55fab76
David Blot <d.nguyenblot@gmail.com>
2023-08-12 20:56:18 -0500

    restructuration des fonctions + ajout addEventListener + proposition de mots

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
 // appel de la fonction pour lancer le jeu :
 lancerJeu();
-
-// let inputEcriture = document.getElementById('inputEcriture');
-// let btnValiderMot = document.getElementById('btnValiderMot');
-
-// let zoneProposition = document.querySelector('.zoneProposition');
-// let spanScore = document.querySelector('.zoneScore span');
-
-// let listeBtnRadio = document.querySelectorAll('.optionSource input');
--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,42 @@
 // fonctions :
-function afficherResultat(score, nombreMotsProposes) {
+const afficherResultat = (score, nombreMotsProposes) => {
 	// Récupération de la zone dans laquelle on va écrire le score
 	let spanScore = document.querySelector('.zoneScore span');
 	// Ecriture du texte
 	let affichageScore = `${score} / ${nombreMotsProposes}`;
 	// On place le texte à l'intérieur du span.
 	spanScore.innerText = affichageScore;
-}
+};
 
-function choisirPhrasesOuMots() {
-	// déclaration de la variable choix de jeu de l'utilisateur :
-	let choix = prompt(
-		"Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?"
-	);
-	// Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix :
-	while (choix !== 'mots' && choix !== 'phrases') {
-		choix = prompt(
-			"Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?"
-		);
-	}
-	return choix;
-}
+const afficherProposition = (proposition) => {
+	let zoneProposition = document.querySelector('.zoneProposition');
+	zoneProposition.innerText = proposition;
+};
 
-function lancerBoucleDeJeu(listePropositions) {
-	let score = 0;
-	// boucle sur les tableaux en fonction du choix utilisateur :
-	for (let i = 0; i < listePropositions.length; i++) {
-		let motUtilisateur = prompt(`Entrez le mot : ${listePropositions[i]}`);
-		if (motUtilisateur === listePropositions[i]) {
-			score++;
-		}
-	}
-
-	return score;
-}
-
-function lancerJeu() {
-	// on demande d'abord à l'utilisateur de choisir avec quelle liste il veut jouer :
-	let choix = choisirPhrasesOuMots();
+const lancerJeu = () => {
 	// déclaration des variables 'score' et 'nombreMotsProposes initialisées à 0 :
 	let score = 0;
-	let nombreMotsProposes = 0;
+	let i = 0;
 
-	if (choix === 'mots') {
-		//la variable 'score' prend la valeur retournée par la fonction "lancerBoucleDeJeu" :
-		score = lancerBoucleDeJeu(listeMots);
-		nombreMotsProposes = listeMots.length;
-	} else {
-		score = lancerBoucleDeJeu(listePhrases);
-		nombreMotsProposes = listePhrases.length;
-	}
+	let btnValiderMot = document.getElementById('btnValiderMot');
+	let inputEcriture = document.getElementById('inputEcriture');
 
-	afficherResultat(score, nombreMotsProposes);
-}
+	afficherProposition(listeMots[i]);
+
+	// au click, récupère le mot tapé par l'utilisateur :
+	btnValiderMot.addEventListener('click', () => {
+		console.log(inputEcriture.value);
+		if (inputEcriture.value === listeMots[i]) {
+			score++;
+		}
+		i++;
+		afficherResultat(score, i);
+		inputEcriture.value = '';
+		if (listeMots[i] === undefined) {
+			afficherProposition('Le jeu est fini');
+			btnValiderMot.disabled = true;
+		} else {
+			afficherProposition(listeMots[i]);
+		}
+	});
+};
