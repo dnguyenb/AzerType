47e9f4a5bbffd059838d629f1957fefe31254e6b
David Blot <d.nguyenblot@gmail.com>
2023-08-13 08:41:44 -0500

    ajout commentaires

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@
 const afficherResultat = (score, nombreMotsProposes) => {
 	// Récupération de la zone dans laquelle on va écrire le score
 	let spanScore = document.querySelector('.zoneScore span');
-	// Ecriture du texte
+	// Ecriture du text :
 	let affichageScore = `${score} / ${nombreMotsProposes}`;
-	// On place le texte à l'intérieur du span.
+	// On place le texte à l'intérieur du span :
 	spanScore.innerText = affichageScore;
 };
 
@@ -16,12 +16,12 @@ const afficherProposition = (proposition) => {
 const lancerJeu = () => {
 	// déclaration des variables 'score' et 'nombreMotsProposes initialisées à 0 :
 	let score = 0;
-	let i = 0;
+	let i = 0; // i est le compteur de mots proposés.
 
 	let btnValiderMot = document.getElementById('btnValiderMot');
 	let inputEcriture = document.getElementById('inputEcriture');
 
-	afficherProposition(listeMots[i]);
+	afficherProposition(listeMots[i]); // premier appel de la fonction pour afficher la 1ere proposition.
 
 	// au click, récupère le mot tapé par l'utilisateur :
 	btnValiderMot.addEventListener('click', () => {
@@ -29,14 +29,16 @@ const lancerJeu = () => {
 		if (inputEcriture.value === listeMots[i]) {
 			score++;
 		}
-		i++;
+		i++; // passe au mot suivant.
 		afficherResultat(score, i);
-		inputEcriture.value = '';
+
+		inputEcriture.value = '';// permet de vider le champs à chaque fois que l'on vzlide.
+
 		if (listeMots[i] === undefined) {
 			afficherProposition('Le jeu est fini');
-			btnValiderMot.disabled = true;
+			btnValiderMot.disabled = true;// désactive le bouton "valider".
 		} else {
-			afficherProposition(listeMots[i]);
+			afficherProposition(listeMots[i]); // affiche le mot suivant à chaque click.
 		}
 	});
 };
