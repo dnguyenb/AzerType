ec4795be86a19f162efbf885d8aaa65bc615d841
David Blot <d.nguyenblot@gmail.com>
2023-08-18 15:36:33 -0500

    ajout ecouteur sur btn radio pour le choix de liste

--- src/js/script.js
+++ src/js/script.js
@@ -16,16 +16,17 @@ const lancerJeu = () => {
 	// déclaration des variables 'score' et 'nombreMotsProposes initialisées à 0 :
 	let score = 0;
 	let i = 0; // i est le compteur de mots proposés.
+	let listeProposition = listeMots; // initialisée par défaut sur "listeMots".
 
 	let btnValiderMot = document.getElementById('btnValiderMot');
 	let inputEcriture = document.getElementById('inputEcriture');
 
-	afficherProposition(listeMots[i]); // premier appel de la fonction pour afficher la 1ere proposition.
+	afficherProposition(listeProposition[i]); // premier appel de la fonction pour afficher la 1ere proposition.
 
 	// au click, récupère le mot tapé par l'utilisateur :
 	btnValiderMot.addEventListener('click', () => {
 		console.log(inputEcriture.value);
-		if (inputEcriture.value === listeMots[i]) {
+		if (inputEcriture.value === listeProposition[i]) {
 			score++;
 		}
 		i++; // passe au mot suivant.
@@ -33,11 +34,27 @@ const lancerJeu = () => {
 
 		inputEcriture.value = ''; // permet de vider le champs à chaque fois que l'on vzlide.
 
-		if (listeMots[i] === undefined) {
+		if (listeProposition[i] === undefined) {
 			afficherProposition('Le jeu est fini');
 			btnValiderMot.disabled = true; // désactive le bouton "valider".
 		} else {
-			afficherProposition(listeMots[i]); // affiche le mot suivant à chaque click.
+			afficherProposition(listeProposition[i]); // affiche le mot suivant à chaque click.
 		}
 	});
+
+	// Gestion de l'évènement 'change' sur les boutons radio :
+	let listeBtnRadio = document.querySelectorAll('input[name="optionSource"]');
+
+	for (let i = 0; i < listeBtnRadio.length; i++) {
+		listeBtnRadio[i].addEventListener('change', () => {
+			if (listeBtnRadio[i].value === '1') {
+				listeProposition = listeMots;
+			} else {
+				listeProposition = listePhrases;
+			}
+			// Modifie l'affichage :
+			afficherProposition(listeProposition[i]);
+		});
+	}
+	afficherResultat(score, i);
 };
